a0b6643bd49ea5160e93abc876668cfb7215f301
Vucko98 <milutin.vucko@gmail.com>
2022-08-22 13:52:35 +0200

    Equipment for DataModel

--- ModelLabsProjekat/ModelLabs/NetworkModelService/DataModel/Core/Equipment.cs
+++ ModelLabsProjekat/ModelLabs/NetworkModelService/DataModel/Core/Equipment.cs
@@ -8,11 +8,75 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using FTN.Common;
 
 namespace FTN.Services.NetworkModelService.DataModel.Core
 {
     /// The parts of a power system that are physical devices, electronic or mechanical.
-    public class Equipment : PowerSystemResource {
+    public class Equipment : PowerSystemResource
+    {
+        public Equipment(long globalId) : base(globalId)
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);            
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+        #region IAccess implementation
+
+        public override bool HasProperty(ModelCode property)
+        {
+            return base.HasProperty(property);
+        }
+
+        public override void GetProperty(Property property)
+        {
+            base.GetProperty(property);
+        }
+
+        public override void SetProperty(Property property)
+        {
+            base.SetProperty(property);
+        }
+
+        #endregion IAccess implementation
+
+        #region IReference implementation
+
+        public override bool IsReferenced
+        {
+            get
+            {
+                return base.IsReferenced;
+            }
+        }
+
+        public override void GetReferences(Dictionary<ModelCode, List<long>> references, TypeOfReference refType)
+        {
+            base.GetReferences(references, refType);
+        }
+
+        public override void AddReference(ModelCode referenceId, long globalId)
+        {
+            base.AddReference(referenceId, globalId);
+        }
+
+        public override void RemoveReference(ModelCode referenceId, long globalId)
+        {
+            base.RemoveReference(referenceId, globalId);
+        }
+
+        #endregion IReference implementation
+
     }
 }
+
